ce56cf9f025df4e786b791dbc9552da0c6cdfe45
Ademílson F. Tonato <contato@admcode.com.br>
2016-01-11 20:09:01 -0200

    Update new features

--- app.js
+++ app.js
@@ -1,26 +1,71 @@
-var app = require('app')
-var BrowserWindow = require('browser-window')
-var ipc = require('ipc')
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var ipc = require('ipc');
 
 app.on('ready', function () {
   var mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
-  })
-  mainWindow.loadUrl('file://' + __dirname + '/main.html')
-  mainWindow.openDevTools()
+    height: 600,
+  });
+  mainWindow.loadUrl('file://' + __dirname + '/main.html');
+  
+  //mainWindow.openDevTools()
 
-  var prefsWindow = new BrowserWindow({
+  var defaultWindow = new BrowserWindow({
     width: 400,
     height: 400,
     show: false
-  })
-  prefsWindow.loadUrl('file://' + __dirname + '/prefs.html')
+  });
+  // defaultWindow.loadUrl('file://' + __dirname + '/prefs.html')
 
-  ipc.on('toggle-prefs', function () {
-    if (prefsWindow.isVisible())
-      prefsWindow.hide()
+  // Default
+  ipc.on('toggle-file', function () {
+
+    // Define the windows to open
+    defaultWindow.loadUrl('file://' + __dirname + '/prefs.html');
+
+    if (defaultWindow.isVisible())
+      defaultWindow.hide();
     else
-      prefsWindow.show()
+      defaultWindow.show();
+  })
+
+  // MENU ABOUT
+  ipc.on('toggle-about', function () {
+
+    defaultWindow = new BrowserWindow({
+      width: 400,
+      height: 200,
+      show: false,
+      resizable: false,
+      type: 'notification'
+    })
+
+    // Remove MENU
+    defaultWindow.setMenu(null)
+
+    defaultWindow.loadUrl('file://' + __dirname + '/pages/about.html');
+    
+    if (defaultWindow.isVisible())
+      defaultWindow.hide();
+    else
+      defaultWindow.show();
+  })
+
+/* MENU ABOUT
+ipc.on('toggle-posts', function () {
+
+  defaultWindow.loadUrl('file://' + __dirname + '/pages/posts.html')
+  
+  if (defaultWindow.isVisible())
+    defaultWindow.hide();
+  else
+    defaultWindow.show();
+}) */
+
+  // MENU EXIT
+  ipc.on('toggle-exit', function () {
+
+    mainWindow.destroy();
   })
 })
--- main.js
+++ main.js
@@ -1,18 +1,103 @@
-var remote = require('remote')
-var ipc = require('ipc')
-var Menu = remote.require('menu')
+var fs = require('fs');
+var remote = require('remote');
+var ipc = require('ipc');
+var Menu = remote.require('menu');
+var shell = require('shell');
+var dialog = remote.require('dialog');
+
+var config = require('./config.json');
+var newFolder = null;
+var defaultFolder = null;
 
 var menu = Menu.buildFromTemplate([
+{
+  label: 'File',
+  submenu: [
   {
-    label: 'Electron',
-    submenu: [
-      {
-        label: 'Prefs',
-        click: function () {
-          ipc.send('toggle-prefs')
-        }
+    label: 'New',
+    click: function () {
+      newFolder = dialog.showOpenDialog({title: 'Folder selected', properties: ['openDirectory']});
+
+      if (newFolder === undefined) {
+        console.log('ERROR, please, SELECT THE FOLDER');
+      } else {
+        document.getElementById('default_folder_field').value = newFolder;
+        document.getElementById('form-visible').style.display = 'block';
+
+        alert('roda o comando HARMONIC INIT');
+        getDataDefault(newFolder);
       }
-    ]
+    }
+  },
+  {
+    label: 'Open',
+    click: function () {
+      defaultFolder = dialog.showOpenDialog({title: 'Folder selected', properties: ['openDirectory']});
+
+      document.getElementById('default_folder_field').value = defaultFolder;
+
+      alert('busca o arquivo (verifica se existe), se existir carrega os dados getDataDefault(), do contrário avisa o usuário');
+    }
+  },
+  {
+    label: 'Exit',
+    click: function () {
+      ipc.send('toggle-exit');
+    }
   }
-])
+  ]
+},
+{
+  label: 'Help',
+  submenu: [
+  {
+    label: 'Harmonic',
+    click: function () {
+
+      shell.openExternal(config.external_urls.harmonic);
+    }
+  },
+  {
+    label: 'Twitter',
+    click: function () {
+
+      shell.openExternal(config.external_urls.twitter);
+    }
+  },
+  {
+    label: 'Changelog...',
+    click: function () {
+
+      shell.openExternal(config.external_urls.changelog);
+    }
+  },
+  {
+    label: 'About Harmonic Manager',
+    click: function () {
+      ipc.send('toggle-about');
+    }
+  }
+
+  ]
+}
+]);
 Menu.setApplicationMenu(menu)
+
+function getDataDefault(path) {
+  fs.readFile(path+'/harmonic.json', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    data = JSON.parse(data);
+
+    document.getElementById("nameField").value  = data.name;
+    document.getElementById("titleField").value = data.title;
+    document.getElementById("subtitleField").value = data.subtitle;
+
+    document.getElementById("descriptionField").value = data.description;
+    document.getElementById("authorField").value = data.author;
+    document.getElementById("bioField").value = data.bio;
+    document.getElementById("domainField").value = data.domain;
+  });
+}
